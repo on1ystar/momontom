d9350cb352d924789ad683861785e28254cf54cc
on1ystar <tjdwls0607@naver.com>
2020-12-25 16:12:36 +0900

    Update modal display by using classList

--- todo.js
+++ todo.js
@@ -1,18 +1,15 @@
-function todoCreateClick(){
+function clickTodoCreate(){
     console.log("todo-create click");
-    const modalCreate = document.getElementById("modal-create");
-    modalCreate.style.display = "block";
-    modalCreate.querySelector(".modal-create-form").style.display = "block";
+    const createModal = document.getElementById("modal-create");
+    createModal.classList.remove("hidden");
 }
 
-function todoCreateCancle(){
-    const modalCreate = document.getElementById("modal-create");
-    modalCreate.style.display = "none";
-    modalCreate.querySelector(".modal-create-form").style.display = "none";
-
+function cancleTodoCreate(){
+    const createModal = document.getElementById("modal-create");
+    createModal.classList.add("hidden");
 }
 
-function todoCreateCreate(){
+function createTodoCreate(){
     const todoList = document.querySelector(".todo-list");
     const title = document.getElementsByName("create-title")[0].value;
     const importanceList = document.getElementsByName("create-importance");
@@ -26,19 +23,20 @@ function todoCreateCreate(){
     const description = document.getElementsByName("create-description")[0].value;
     const todo =    `<div class="todo">\
                         <input type="checkbox" name="todo-check">\
+                        <div class="importance" value=${importance} style="display: none;"></div>\
                         <div class="todo-title">${title}</div>\
-                        <div name="importance" value=${importance} style="display: none;"></div>\
                         <div class="todo-duedate">due. ${dueDate}</div>\
-                        <div name="description" style="display: none;">${description}</div>\
+                        <div class="description" style="display: none;">${description}</div>\
                     </div>`
+                   
     todoList.insertAdjacentHTML('beforeend', todo);
-    todoCreateCancle()
+    cancleTodoCreate()
 }
 
-const todoCreate = document.querySelector(".todo-create");
-const modalCancle = document.querySelector(".modal-create-form .btn-cancle");
-const modalCreate = document.querySelector(".modal-create-form .btn-create");
+const btnTodoCreate = document.querySelector(".todo-create");
+const btnCancleModal = document.querySelector(".modal-create-form .btn-cancle");
+const btnCreateModal = document.querySelector(".modal-create-form .btn-create");
 
-todoCreate.addEventListener("click", todoCreateClick);
-modalCancle.addEventListener("click", todoCreateCancle);
-modalCreate.addEventListener("click", todoCreateCreate);
+btnTodoCreate.addEventListener("click", clickTodoCreate);
+btnCancleModal.addEventListener("click", cancleTodoCreate);
+btnCreateModal.addEventListener("click", createTodoCreate);
